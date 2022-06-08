a508adbc6afe2cba399c799c435bdc822b75a99c
Max Katz <maxkatz6@outlook.com>
2022-06-08 02:39:22 -0400

    Android implementation

--- src/Avalonia.Base/Logging/LogArea.cs
+++ src/Avalonia.Base/Logging/LogArea.cs
@@ -44,6 +44,11 @@ namespace Avalonia.Logging
         /// The log event comes from X11Platform.
         /// </summary>
         public const string X11Platform = nameof(X11Platform);
+
+        /// <summary>
+        /// The log event comes from AndroidPlatform.
+        /// </summary>
+        public const string AndroidPlatform = nameof(AndroidPlatform);
         
         /// <summary>
         /// The log event comes from IOSPlatform.
